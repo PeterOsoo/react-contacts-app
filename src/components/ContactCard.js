51d23d0d199a31d95e980308d91576796b8f8ab3
Peter Osoo <posoo617@gmail.com>
2023-02-18 11:45:40 +0300

    clean up

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,18 +1,21 @@
 import React from "react"
+import user from "../images/user.webp"
 
 export default function ContactCard(props) {
-	const { id, name, email } = props.contact
+	const { name, email } = props.contact
+	const fontSize = "fontSize: '150%'"
 
 	return (
-		<div className="item">
+		<div className="item" style={{ fontSize }}>
+			<img src={user} alt="Avatar" className="ui avatar image" />
+
 			<div className="content">
 				<div className="header"> {name} </div>
 				<div> {email} </div>
 			</div>
-
 			<i
 				className="trash alternate icon outline"
-				style={{ color: "red", marginTop: "7px" }}
+				style={{ color: "red", marginTop: "7px", float: "right" }}
 			></i>
 		</div>
 	)
