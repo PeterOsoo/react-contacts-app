0e30cf5a96ac08143b399dd34d8f934ca4b10e05
Peter Osoo <posoo617@gmail.com>
2023-02-18 11:09:01 +0300

    add contact

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
 export default function AddContact() {
-  const onSubmit = (e) => {
-    e.preventDefault()
-    console.log("Button clicked ");
-  }
-
+  
   return (
     <div> 
         <div className="ui main">
           <h2>Add Contact</h2>
           <form action="" className="ui form">
+
             <div className="field">
               <label htmlFor="">Name</label>
               <input type="text" name="name" id="" placeholder='Name' />
             </div>
+            
+            <div className="field">
+              <label htmlFor="">Email</label>
+              <input type="text" name="email" id="" placeholder='Email' />
+            </div>
+
+            <button className="ui button blue">Add</button>
+
           </form>
         </div>
     </div>
