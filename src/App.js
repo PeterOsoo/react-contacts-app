43dc5d1556ea570b9602a85d7ecce7a0a040830e
Peter Osoo <posoo617@gmail.com>
2023-02-19 17:11:37 +0300

    use State hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import "./App.css"
+import { useState } from "react"
+
 import Header from "./components/Header"
 import AddContact from "./components/AddContact"
 import ContactList from "./components/ContactList"
 
 const App = () => {
-	const contacts = [
-		{
-			id: 1,
-			name: "Jaka",
-			email: "Jaka@email.com",
-		},
-		{
-			id: 2,
-			name: "Blak",
-			email: "Blak@email.com",
-		},
-	]
+	const [contacts, setContacts] = useState([])
 	return (
 		<div className="ui container">
 			<Header />
